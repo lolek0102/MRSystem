10a74019f54ce25d50b7e5d78cba36b0aeab99d4
Karol Kowalski <karol.kowalski@microsoft.wsei.edu.pl>
2023-06-18 14:47:02 +0200

    signup window created signup design started loginwindow button signup logic added drag feature for signup window added

--- MRSystem/SignUp.xaml.cs
+++ MRSystem/SignUp.xaml.cs
@@ -10,43 +10,29 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace MRSystem
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logika interakcji dla klasy SignUp.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SignUp : Window
     {
-        public MainWindow()
+        public SignUp()
         {
             InitializeComponent();
-        }
-
-
-        private void btnLogin_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnSignup_Click(object sender, RoutedEventArgs e)
-        {
-            this.Hide();
-            SignUp sgn = new SignUp();
-            sgn.Show();
-        }
-
-        private void BtnClose_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
         }
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
-                this.DragMove();
+            this.DragMove();
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
