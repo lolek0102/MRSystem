7424148d8eaae26cbdfeeb484f702a53c5f85999
Karol Kowalski <karol.kowalski@microsoft.wsei.edu.pl>
2023-06-24 16:47:50 +0200

    DataGrid logic

--- MRSystem/MainPanel.xaml.cs
+++ MRSystem/MainPanel.xaml.cs
@@ -14,19 +14,31 @@ using System.Windows.Shapes;
 
 namespace MRSystem
 {
+
     /// <summary>
     /// Logika interakcji dla klasy MainPanel.xaml
     /// </summary>
     public partial class MainPanel : Window
     {
+
+        readonly string connectionString = @"Data Source=localhost;Initial Catalog=movie_rental;Integrated Security=True;TrustServerCertificate=true";
         public MainPanel()
         {
             InitializeComponent();
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (MRdbContext db = new MRdbContext(connectionString))
+            {
+                var query = from mv in db.Movies select new { mv.Id, mv.Title, mv.Director, mv.Genre, mv.MovieStudio };
+                dgMovieView.ItemsSource = query.ToList();
+            }
+
+        }
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            
         }
 
         private void borrowBtn_Click(object sender, RoutedEventArgs e)
