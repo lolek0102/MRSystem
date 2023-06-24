d1a136a7f61ec95dc9951a36aeb57ed5024d4006
Karol Kowalski <karol.kowalski@microsoft.wsei.edu.pl>
2023-06-24 17:33:19 +0200

    borrow panel design part 1

--- MRSystem/BorrowWindow.xaml.cs
+++ MRSystem/BorrowWindow.xaml.cs
@@ -23,5 +23,15 @@ namespace MRSystem
         {
             InitializeComponent();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
